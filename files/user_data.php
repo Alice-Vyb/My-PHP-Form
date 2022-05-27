e7faca06e022ad469f3a05d66c1a4131c5f58653
Ogbonna alice <vicktoriitory@gmail.com>
2022-05-27 02:30:37 +0100

    Added php functionality

---
+++ files/user_data.php
@@ -0,0 +1,22 @@
+<?php
+   
+     $name = $_POST['name'];
+    $email = $_POST['email'];
+    $country = $_POST['country'];
+    $d_o_b = $_POST['d_o_b'];
+    $gender = $_POST['gender'];
+    $data = [$name, $email, $country, $d_o_b, $gender];
+    
+    foreach($data as $value){
+      print_r("$value <br>");
+    }
+   
+$file = "userdata.csv";
+$handle = fopen("userdata.csv", "a");
+
+fputcsv($handle, $data);
+
+fclose($handle);
+
+
+?>
